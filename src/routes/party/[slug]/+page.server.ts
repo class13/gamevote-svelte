0167f585539cac1d32f70ea9fdf36858ad904745
class13 <104397666+lionell-tyler@users.noreply.github.com>
2023-02-10 18:42:37 +0100

    select what i want to do next

--- src/routes/party/[slug]/+page.server.ts
+++ src/routes/party/[slug]/+page.server.ts
@@ -28,14 +28,17 @@ export async function load({params, locals, cookies}: Params) {
 }
 // todo: implement deletion of options
 // todo: implement starting of vote
-// todo: implement voting
-// todo: implement user token (include party so username is only unique for party)
+
+// todo: implement voting <
+
 // todo: implement a socket to connect to, to get a live option and attendee list
 // todo: when entering a party first time need to register always
+// todo: include party in payload of jwt token (otherwise it might be used in another party)
 export const actions = {
     addOption: async ({locals, params, request}: Params) => {
         let data = await request.formData()
         data = Object.fromEntries(data.entries())
         await locals.api.post(`/parties/${params.slug}/options` , {value: data.option})
+        throw redirect(302, `/party/${params.slug}`)
     }
 }
