4d7fb27658f57778def0b6f81a449290540cd2ff
lucky <wiiinator1@gmail.com>
2026-09-21 23:35:57 +0200

    Revert "Show recent parties on the start page"
    This reverts commit b74a3b03e6b9d33fa733a44546310653dd66bedc.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,12 +3,6 @@ import {getContext, setContext} from "svelte";
 import {ApiClient} from "../lib/apiclient/apiclient"
 import type {Locals} from "../hooks.server";
 
-export async function load({locals}: {locals: Locals}) {
-    return {
-        recentParties: await locals.apiclient.get("/parties/recent")
-    }
-}
-
 export const actions = {
     createparty: async function({locals}: {locals: Locals}) {
         let party = await locals.apiclient.post("/parties", {})
